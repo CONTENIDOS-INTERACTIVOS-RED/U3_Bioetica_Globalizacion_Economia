1d03156302be023668bf44e31810502142889a67
flizcanor <flizcanor@icloud.com>
2024-12-23 11:44:28 -0500

    Avances tema 2

--- src/views/curso/Tema2.vue
+++ src/views/curso/Tema2.vue
@@ -76,6 +76,79 @@
         figure
           img(src="@/assets/curso/tema2/5.png", alt="").mt-4.mt-lg-0
 
+    .row.align-items-center.mb-5
+      .col-lg-7.order-2
+        AcordionA(tipo="b")
+          .div(titulo="Acuerdo de París")
+            p El Acuerdo de París es un acuerdo dentro del marco de la Convención Marco de las Naciones Unidas, sobre el Cambio Climático, que establece medidas para la reducción de las emisiones de gases de efecto invernadero.
+
+          .div(titulo="ISO 14001 Gestión Ambiental")
+            p Establece los criterios para un sistema de gestión ambiental efectivo, que ayuda a las organizaciones a mejorar su desempeño ambiental.
+          
+          .div(titulo="ISO 26000 Responsabilidad Social")
+            p Proporciona directrices sobre cómo las organizaciones pueden operar de manera socialmente responsable, promoviendo la sostenibilidad y el respeto de los derechos humanos.
+
+
+      .col-lg-5.order-1
+        figure
+          img(src="@/assets/curso/tema2/6.png", alt="").mt-4.mt-lg-0
+
+
+    p(data-aos="fade-left").mb-5 Colombia, como parte de la comunidad internacional, ha adoptado diversos tratados, convenciones y políticas, para alinearse con las normativas internacionales en materia de derechos humanos y recursos naturales. A nivel local, el país también tiene un robusto marco normativo que busca proteger tanto a las personas como al medio ambiente:
+
+
+    .row.align-items-center.mb-5
+      .col-lg-7
+        AcordionA(tipo="b")
+          .div(titulo="1991. Constitución Política de Colombia")
+            p Reconoce una serie de derechos fundamentales que incluyen el derecho a la vida, a la salud, a un ambiente sano, a la educación y a la propiedad. En su artículo 79, la Constitución establece el derecho de todas las personas a gozar de un ambiente sano y la obligación del Estado de proteger la biodiversidad.
+
+          .div(titulo="Ley 1448 del 2011. Ley de Víctimas y Restitución de Tierras")
+            p Reconoce los derechos de las víctimas del conflicto armado y establece mecanismos de restitución de tierras, lo cual tiene implicaciones importantes en la protección de los recursos naturales, en áreas afectadas por el conflicto.
+
+
+      .col-lg-5
+        figure
+          img(src="@/assets/curso/tema2/7.png", alt="").mt-4.mt-lg-0
+
+    .row.align-items-center.mb-5
+      .col-lg-7.order-2
+        AcordionA(tipo="b")
+          .div(titulo="Ley 99 de 1993. Ministerio del Medio Ambiente")
+            p Se creó el Ministerio del Medio Ambiente (hoy conocido como el Ministerio de Ambiente y Desarrollo Sostenible) y establece el marco normativo para la protección de los recursos naturales en Colombia.
+
+          .div(titulo="2016. Sentencia T-622")
+            p Reconoció el derecho fundamental a la paz y al medio ambiente, afirmando que las comunidades afectadas por la minería ilegal y el extractivismo tienen el derecho a vivir en un ambiente libre de contaminación.
+          
+          .div(titulo="2014. Ley 1715 Energías Renovables")
+            p Establece un marco normativo para el aprovechamiento de fuentes no convencionales de energía, como la solar y eólica, en un esfuerzo por reducir la dependencia de recursos naturales no renovables y promover la sostenibilidad.
+
+      .col-lg-5.order-1
+        figure
+          img(src="@/assets/curso/tema2/8.png", alt="").mt-4.mt-lg-0
+
+
+    .row.justify-content-center.align-items-center.mb-5
+      .col-lg-12
+        .bg-color-4.p-4(data-aos="fade-left")
+          .row.align-items-center
+            .col-lg-auto
+              img(src="@/assets/curso/tema2/9.svg", style="max-width: 90px").mx-auto
+            .col-lg
+              p.mb-0 Tanto a nivel internacional como en Colombia, existe una legislación robusta y en constante evolución, para proteger los derechos humanos y los recursos naturales. Sin embargo, los desafíos persisten y es fundamental seguir impulsando políticas públicas que integren el respeto por los derechos fundamentales y la sostenibilidad ambiental.
+
+
+    #t_2_3.titulo-segundo(data-aos="flip-up").mb-5
+      h2 #[span 2.3] Responsabilidad social de las empresas en la protección de recursos naturales
+
+    .row.mb-4
+      .col-lg-8
+        p(data-aos="fade-right") En la unidad anterior, se abordó la Responsabilidad Social Corporativa y ética en las decisiones empresariales, viendo la importancia de nuevamente resaltar el papel protagónico de las organizaciones, porque estas deben adoptar una postura ética, comprometiéndose a una gestión eficiente de los recursos, a la reducción de su huella ecológica y al respeto por los derechos humanos y el bienestar de las comunidades. La RSC y la ética en los negocios, no son solo un imperativo moral, sino también una estrategia empresarial inteligente que contribuye a la sostenibilidad a largo plazo y a la creación de valor tanto para la sociedad como para la empresa misma. Esto es posible a través de innovación tecnológica y prácticas sostenibles.
+      .col-lg-4
+        figure
+          img(src="@/assets/curso/tema2/10.png", data-aos="zoom-in").mt-4.mt-lg-0
+
+    p(data-aos="fade-left").mb-5 Para que una empresa se convierta en un "buen ciudadano corporativo", es decir, que destine recursos y esfuerzos para mejorar la calidad de vida de la comunidad, en su conjunto, debe cumplir previamente con una serie de responsabilidades fundamentales. Estas responsabilidades se encuentran reflejadas en la pirámide de Carroll, un modelo que establece una jerarquía de obligaciones que las empresas deben asumir de manera progresiva:
 
     .bg-full-width.border-top.color-primario
       .p-4.p-md-5
